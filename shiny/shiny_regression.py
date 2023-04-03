0e7d29c9956d17654d1c3a9974ed6bde17340712
Akhil Garg <31163077+A-Garg@users.noreply.github.com>
2023-04-02 23:10:52 -0400

    Update shiny_regression.py
    Combine all regression models into one file

--- shiny/shiny_regression.py
+++ shiny/shiny_regression.py
@@ -1,3 +1,12 @@
+##################################
+#                                #
+# shiny_regression.py            #
+# Created 2023-03-11             #
+# Akhil Garg, akhil@akhilgarg.ca #
+#                                #
+##################################
+
+
 from shiny import App, render, ui
 import pickle
 import pandas as pd
@@ -9,7 +18,8 @@ antibiotic_dict = {'Pencillins':     {'Piptaz':'Piperacillin-tazobactam'},
                    'Carbapenems':    {'Meropenem':'Meropenem'},
                    'Quinolones':     {'Ciprofloxacin':'Ciprofloxacin'},
                    'Aminoglycosides':{'Tobramycin':'Tobramycin'},
-                   'Other':          {'TMPSMX':'Trimethoprim-sulfamethoxazole'}
+                   'Other':          {'TMPSMX':'Trimethoprim-sulfamethoxazole'},
+                   'Combinations':   {'Piptaz_or_Tobramycin':'Piperacillin-tazobactam and tobramycin'}
                   }
 
 antibiotic_classes = {'Cefazolin':'Cephalosporin','Ceftriaxone':'Cephalosporin','Ceftazidime':'Cephalosporin',
@@ -31,7 +41,12 @@ age_categories = {
 12:'>90'}  
 
 app_ui = ui.page_fluid(
-    ui.h2("Antimicrobial regression models"),
+    ui.h1("Antimicrobial regression models"),
+    
+    ui.h2('Output'),
+    ui.output_text_verbatim('output'),
+    
+    ui.h2('Input'),
     ui.input_select('antibiotic', 'Antibiotic', antibiotic_dict),
     # ui.input_numeric('age', 'Age:', 0, min=18, max=110),
     ui.input_select('Age', 'Age:', age_categories),
@@ -40,12 +55,27 @@ app_ui = ui.page_fluid(
     ui.input_radio_buttons('ClinicalESBL','Clinical ESBL:',[0,1],inline=True),
     ui.input_radio_buttons('RecentHospitalization','Recent Hospitalization:',[0,1],inline=True),
     ui.input_radio_buttons('ICUExposure','ICU exposure:',[0,1],inline=True),
-    ui.input_radio_buttons('PriorGNresistance','Prior gram negative resistance:',[0,1],inline=True),
-    ui.input_radio_buttons('PriorSameClassAbx','Prior antibiotic from same class:',[0,1],inline=True),
-    ui.input_radio_buttons('PriorDiffClassAbx','Prior antibiotic from different class:',[0,1],inline=True),
     ui.input_radio_buttons('MedVsSurgAdmission','Admission type',{0:'Medical',1:'Surgical'},inline=True),
     
-    ui.output_text_verbatim('output')
+    # Single antibiotic choices
+    ui.panel_conditional("input.antibiotic != 'Piptaz_or_Tobramycin'",
+    
+        ui.input_radio_buttons('PriorSameClassAbx','Prior antibiotic from same class:',[0,1],inline=True),
+        ui.input_radio_buttons('PriorDiffClassAbx','Prior antibiotic from different class:',[0,1],inline=True),
+    ),
+
+    # Combination antibiotic choices
+    ui.panel_conditional("input.antibiotic == 'Piptaz_or_Tobramycin'",
+    
+        ui.input_radio_buttons('PriorPenicillin','Prior penicillin exposure:',[0,1],inline=True),
+        ui.input_radio_buttons('PriorAMG','Prior aminoglycoside exposure:',[0,1],inline=True),
+        ui.input_radio_buttons('PriorNonPenicillin','Prior non-penicillin antibiotic exposure:',[0,1],inline=True),
+        ui.input_radio_buttons('PriorNonAMG','Prior non-aminoglycoside antibiotic exposure:',[0,1],inline=True)
+    ),
+    
+    ui.input_radio_buttons('PriorGNresistance','Prior gram negative resistance:',[0,1],inline=True),
+    ui.em('If the patient has had a previous gram negative organism isolated, pick 0 if the previous one was susceptible, or 1 if resistant or unknown. If no previous gram negative organism was isolated, pick 0.'),
+    ui.br('')
 )
 
 
@@ -56,29 +86,64 @@ def server(input, output, session):
     def output():
         '''Main output'''
         
-        with open('../pickle_files/'+input.antibiotic()+'.pickle','rb') as f:
-            reg = pickle.load(f)
-                
-        regression_inputs = ['Age','SexCat','MedVsSurgAdmission','ClinicalESBL',
-                             'RecentHospitalization','ICUExposure','PriorGNresistance',
-                             'Prior'+antibiotic_classes[input.antibiotic()],
-                             'PriorNon'+antibiotic_classes[input.antibiotic()]]
-        regression_values = [input.Age(),
-                             input.SexCat(),
-                             input.MedVsSurgAdmission(),
-                             input.ClinicalESBL(),
-                             input.RecentHospitalization(),
-                             input.ICUExposure(),
-                             input.PriorGNresistance(),
-                             input.PriorSameClassAbx(),
-                             input.PriorDiffClassAbx()]
-                             
-        df = pd.DataFrame(columns=regression_inputs)
-        df.loc[len(df)] = regression_values
+        # Case of a single antibiotic
+        if input.antibiotic() != 'Piptaz_or_Tobramycin':
         
-        susceptibility = reg.predict_proba(df)[0][0]
+            with open('../pickle_files/'+input.antibiotic()+'.pickle','rb') as f:
+                reg = pickle.load(f)
+                    
+            regression_inputs = ['Age','SexCat','MedVsSurgAdmission','ClinicalESBL',
+                                 'RecentHospitalization','ICUExposure','PriorGNresistance',
+                                 'Prior'+antibiotic_classes[input.antibiotic()],
+                                 'PriorNon'+antibiotic_classes[input.antibiotic()]]
+            regression_values = [input.Age(),
+                                 input.SexCat(),
+                                 input.MedVsSurgAdmission(),
+                                 input.ClinicalESBL(),
+                                 input.RecentHospitalization(),
+                                 input.ICUExposure(),
+                                 input.PriorGNresistance(),
+                                 input.PriorSameClassAbx(),
+                                 input.PriorDiffClassAbx()]
+                                 
+            df = pd.DataFrame(columns=regression_inputs)
+            df.loc[len(df)] = regression_values
+            
+            susceptibility = reg.predict_proba(df)[0][0]
 
-        return ('''Antibiotic:                    {}
+            return ('''Antibiotic:                    {}
 Probability of susceptibility: {:.1f}%'''.format(input.antibiotic(),susceptibility*100))
+        
+        # Case of a combination antibiotic
+        else: 
+            
+            regression_inputs = ['Age','SexCat','MedVsSurgAdmission','ClinicalESBL',
+                                 'RecentHospitalization','ICUExposure',
+                                 'PriorGNresistance',
+                                 'PriorPenicillin','PriorAMG','PriorNonPenicillin','PriorNonAMG']
+            with open('../pickle_files/'+input.antibiotic()+'.pickle','rb') as f:
+                reg = pickle.load(f)
+                
 
+            regression_values = [input.Age(),
+                                 input.SexCat(),
+                                 input.MedVsSurgAdmission(),
+                                 input.ClinicalESBL(),
+                                 input.RecentHospitalization(),
+                                 input.ICUExposure(),
+                                 input.PriorGNresistance(),
+                                 input.PriorPenicillin(),
+                                 input.PriorAMG(),
+                                 input.PriorNonPenicillin(),
+                                 input.PriorNonAMG()
+                                ]
+                             
+            df = pd.DataFrame(columns=regression_inputs)
+            df.loc[len(df)] = regression_values
+            
+            susceptibility = reg.predict_proba(df)[0][0]                     
+            return ('''Antibiotics:                   {}
+Probability of susceptibility: {:.1f}%'''.format(input.antibiotic(),susceptibility*100))
+            print('doeo')
+        
 app = App(app_ui, server)
