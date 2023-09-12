8de85d9b85dd357ef147efe9baff8df128a64c4c
Akhil Garg <31163077+A-Garg@users.noreply.github.com>
2023-09-11 22:54:47 -0400

    Add files via upload
    Updated most files, now with a functioning shiny app!

--- README.md
+++ README.md
@@ -7,67 +7,27 @@ Creating a model of antimicrobial susceptibility based on local risk factors
 * `antimicrobial_imputation.ipynb`: Uses imputation rules to fill in antimicrobial susceptibility patterns (e.g. methicillin-susceptible *S. aureus* should also be susceptible to piperacillin-tazobactam).
 * `missingness.ipynb`: Calculates the amount of missing susceptibility data for each antibiotic
 * `regression.ipynb`: The actual logistic regression model for antibiotic susceptibility
-* `regression2.ipynb`: A shorter version of `regression.ipynb` that suppresses some output for easier readability
-
-
-### Pickle files
-For each antibiotic or combination antibiotic, there is a [pickle](https://docs.python.org/3/library/pickle.html) file that stores the regression model so it can be used elsewhere.
-
-
-### Shiny files
-* `shiny_regression.py`: creates a web interface from which to use the regression models
-
 
 ### Other Files
 * `AMR_imputation_6.xlsx`: Source data
+* `regression.html`: iPython notebook with figure visualizations
 
 ## Example output
 
-### Regression model
-
 ```
 Regression model for Cefazolin
-
 Logistic Regression Coefficients
-PriorGNresistance                           1.4911
-ClinicalESBL                                1.0636
-PriorCephalosporin                          0.5474
-ICUExposure                                 0.3696
-MedVsSurgAdmission                          0.3324
-PriorNonCephalosporin                       0.1171
-Age                                        -0.0567
-RecentHospitalization                      -0.1624
-SexCat                                     -0.4152
 
-Probability of susceptibility: prediction outputs
-Susceptible isolates min:     0.07
-                     median:  0.68
-                     mean:    0.65
-                     max:     0.80
-                     
-Resistant isolates   min:     0.04
-                     median:  0.51
-                     mean:    0.47
-                     max:     0.79
-                     
-
-Bin minimum   S count  R count  S percentage
-       0.00         1       18          0.05
-       0.10         1       32          0.03
-       0.20         5       24          0.17
-       0.30        19       32          0.37
-       0.40        14       33          0.30
-       0.50        42       43          0.49
-       0.60       102       61          0.63
-       0.70       121       45          0.73
-       0.80         3        0          1.00
-       0.90         0        0           nan
+PriorGNinf                                  1.4891
+ClinicalESBL                                1.0489
+PriorCephalosporin                          0.5478
+ICUExposure                                 0.3666
+Medical(1) Surgical (2) Admitting Service   0.3334
+ClincalMRSA                                 0.1141
+PriorNonCephalosporin                       0.1135
+ClinicalVRE                                 0.0000
+Age                                        -0.0571
+RecentHospitalization                      -0.1632
+SexCat                                     -0.4171`
 ```
-![Cefazolin output](https://user-images.githubusercontent.com/31163077/198854338-c4f57208-880c-4b2d-ab50-9af27394b30d.png)
-
-
-### Shiny
-![image](https://user-images.githubusercontent.com/31163077/228419993-8e4012fa-b8d4-4b5b-97e9-61252aff7832.png)
-
-
-
+![Ceftriaxone Output](https://user-images.githubusercontent.com/31163077/182207506-6cba5e0c-4dfa-40fc-bd01-fb316e8af6a1.png)
--- shiny/shiny_regression.py
+++ shiny/shiny_regression.py
@@ -10,8 +10,15 @@
 from shiny import App, render, ui
 import pickle
 import pandas as pd
+import matplotlib.pyplot as plt
 
-antibiotic_list = ['Cefazolin','Ceftriaxone','Ceftazidime','Piptaz','Meropenem','Ciprofloxacin','Tobramycin','TMPSMX']
+
+# Common variables used throughout
+antibiotic_list = ['Cefazolin','Ceftriaxone','Ceftazidime',
+                   'Piptaz','Meropenem','Ciprofloxacin',
+                   'Tobramycin','TMPSMX',
+                   'Piptaz_or_Tobramycin'
+                  ]
 
 antibiotic_dict = {'Pencillins':     {'Piptaz':'Piperacillin-tazobactam'},
                    'Cephalosporins': {'Cefazolin':'Cefazolin','Ceftriaxone':'Ceftriaxone','Ceftazidime':'Ceftazidime'},
@@ -24,8 +31,20 @@ antibiotic_dict = {'Pencillins':     {'Piptaz':'Piperacillin-tazobactam'},
 
 antibiotic_classes = {'Cefazolin':'Cephalosporin','Ceftriaxone':'Cephalosporin','Ceftazidime':'Cephalosporin',
                       'Piptaz':'Penicillin','Meropenem':'Carbapenem','Ciprofloxacin':'FQ','Tobramycin':'AMG',
-                      'TMPSMX':'OtherAbx'}
+                      'TMPSMX':'OtherAbx'
+                     }
     
+regression_inputs = ['Hospital','Age','SexCat','MedVsSurgAdmission',
+                     'RecentHospitalization','ICUExposure',
+                     'PriorPenicillin','PriorCephalosporin','PriorCarbapenem',
+                     'PriorFQ','PriorAMG','PriorOtherAbx',                     
+                     'PriorCefazolinResistance', 'PriorCeftriaxoneResistance', 
+                     'PriorCeftazidimeResistance', 'PriorPiptazResistance', 
+                     'PriorMeropenemResistance', 'PriorCiprofloxacinResistance', 
+                     'PriorTobramycinResistance', 'PriorTMPSMXResistance',
+                     'ClinicalESBL'
+                    ]
+
 age_categories = {
 1: '<40', 
 2: '40-44',
@@ -41,109 +60,211 @@ age_categories = {
 12:'>90'}  
 
 app_ui = ui.page_fluid(
-    ui.h1("Antimicrobial regression models"),
+    ui.h1("Antibiotic regression models"),
     
-    ui.h2('Output'),
-    ui.output_text_verbatim('output'),
-    
-    ui.h2('Input'),
-    ui.input_select('antibiotic', 'Antibiotic', antibiotic_dict),
-    # ui.input_numeric('age', 'Age:', 0, min=18, max=110),
-    ui.input_select('Age', 'Age:', age_categories),
+    ui.layout_sidebar(
+        
+        sidebar=ui.panel_sidebar(
+            ui.h3('Input'),
+            
+            # ui.input_select('antibiotic', 'Antibiotic', antibiotic_dict),
+            # ui.input_numeric('age', 'Age:', 0, min=18, max=110),
 
-    ui.input_radio_buttons('SexCat','Sex:',{0:'Male',1:'Female'},inline=True),
-    ui.input_radio_buttons('ClinicalESBL','Clinical ESBL:',[0,1],inline=True),
-    ui.input_radio_buttons('RecentHospitalization','Recent Hospitalization:',[0,1],inline=True),
-    ui.input_radio_buttons('ICUExposure','ICU exposure:',[0,1],inline=True),
-    ui.input_radio_buttons('MedVsSurgAdmission','Admission type',{0:'Medical',1:'Surgical'},inline=True),
-    
-    # Single antibiotic choices
-    ui.panel_conditional("input.antibiotic != 'Piptaz_or_Tobramycin'",
-    
-        ui.input_radio_buttons('PriorSameClassAbx','Prior antibiotic from same class:',[0,1],inline=True),
-        ui.input_radio_buttons('PriorDiffClassAbx','Prior antibiotic from different class:',[0,1],inline=True),
-    ),
+            ui.input_radio_buttons('Hospital','Hospital:',{0:'Sunnybrook',1:'TOH'},inline=True),
 
-    # Combination antibiotic choices
-    ui.panel_conditional("input.antibiotic == 'Piptaz_or_Tobramycin'",
-    
-        ui.input_radio_buttons('PriorPenicillin','Prior penicillin exposure:',[0,1],inline=True),
-        ui.input_radio_buttons('PriorAMG','Prior aminoglycoside exposure:',[0,1],inline=True),
-        ui.input_radio_buttons('PriorNonPenicillin','Prior non-penicillin antibiotic exposure:',[0,1],inline=True),
-        ui.input_radio_buttons('PriorNonAMG','Prior non-aminoglycoside antibiotic exposure:',[0,1],inline=True)
+            ui.h4('Demographic factors'),
+
+            ui.input_select('Age', 'Age:', age_categories),
+            ui.input_radio_buttons('SexCat','Sex:',{0:'Male',1:'Female'},inline=True),
+
+            ui.h4('Clinical factors'),
+            
+            ui.input_radio_buttons('RecentHospitalization','Recent Hospitalization:',[0,1],inline=True),
+            ui.input_radio_buttons('ICUExposure','ICU exposure:',[0,1],inline=True),
+            ui.input_radio_buttons('MedVsSurgAdmission','Admitting service',{0:'Medical',1:'Surgical'},inline=True),
+            
+            ui.h4('Antimicrobial exposures'),
+
+            ui.input_radio_buttons('PriorPenicillin','Prior penicillin:',[0,1],inline=True),    
+            ui.input_radio_buttons('PriorCephalosporin','Prior cephalosporin:',[0,1],inline=True),    
+            ui.input_radio_buttons('PriorCarbapenem','Prior carbapenem:',[0,1],inline=True),    
+            ui.input_radio_buttons('PriorFQ','Prior fluoroquinolone:',[0,1],inline=True),    
+            ui.input_radio_buttons('PriorAMG','Prior aminoglycoside:',[0,1],inline=True),    
+            ui.input_radio_buttons('PriorOtherAbx','Prior other antibiotic:',[0,1],inline=True), 
+
+            ui.h4('Resistance history'),
+            
+            ui.em('Referring specifically to ',ui.strong('gram negative'),' resistance.\n'),
+            
+            ui.input_radio_buttons('PriorCefazolinResistance','Prior cefazolin resistance:',[0,1],inline=True),    
+            ui.input_radio_buttons('PriorCeftriaxoneResistance','Prior ceftriaxone resistance:',[0,1],inline=True),    
+            ui.input_radio_buttons('PriorCeftazidimeResistance','Prior ceftazidime resistance:',[0,1],inline=True),    
+            ui.input_radio_buttons('PriorPiptazResistance','Prior piperacillin-tazobactam resistance:',[0,1],inline=True),    
+            ui.input_radio_buttons('PriorMeropenemResistance','Prior meropenem resistance:',[0,1],inline=True),    
+            ui.input_radio_buttons('PriorCiprofloxacinResistance','Prior ciprofloxacin resistance:',[0,1],inline=True),    
+            ui.input_radio_buttons('PriorTobramycinResistance','Prior tobramycin resistance:',[0,1],inline=True),    
+            ui.input_radio_buttons('PriorTMPSMXResistance','Prior trimethoprim-sulfamethoxazole resistance:',[0,1],inline=True),    
+           
+            ui.input_radio_buttons('ClinicalESBL','Clinical ESBL:',[0,1],inline=True),
+        ),
+        
+        main=ui.panel_sidebar(
+        
+            ui.h3('Output'),
+            ui.output_data_frame('predicted_susceptibilities'),
+            # Clinical severity cutoffs: 80% if qSOFA<=2, 90% if qSOFA=3
+            ui.input_radio_buttons('Severity','Clinical severity (affects 80 vs 90% cutoff):',{80:'qSOFA<=2',90:'qSOFA=3'},inline=True),
+            ui.output_plot('plot'),
+            ui.output_text_verbatim('output'),
+            
+        ),        
     ),
     
-    ui.input_radio_buttons('PriorGNresistance','Prior gram negative resistance:',[0,1],inline=True),
-    ui.em('If the patient has had a previous gram negative organism isolated, pick 0 if the previous one was susceptible, or 1 if resistant or unknown. If no previous gram negative organism was isolated, pick 0.'),
     ui.br('')
 )
 
+# Server input
 
 def server(input, output, session):
+ 
     @output
-    @render.text
-    
-    def output():
-        '''Main output'''
+    @render.data_frame
+
+    def predicted_susceptibilities():
         
-        # Case of a single antibiotic
-        if input.antibiotic() != 'Piptaz_or_Tobramycin':
+        susceptibility_outputs = []
         
-            with open('../pickle_files/'+input.antibiotic()+'.pickle','rb') as f:
+        for antibiotic in antibiotic_list:
+            
+            with open(antibiotic+'.pickle','rb') as f:
                 reg = pickle.load(f)
                     
-            regression_inputs = ['Age','SexCat','MedVsSurgAdmission','ClinicalESBL',
-                                 'RecentHospitalization','ICUExposure','PriorGNresistance',
-                                 'Prior'+antibiotic_classes[input.antibiotic()],
-                                 'PriorNon'+antibiotic_classes[input.antibiotic()]]
-            regression_values = [input.Age(),
+            regression_values = [input.Hospital(),
+                                 input.Age(),
                                  input.SexCat(),
                                  input.MedVsSurgAdmission(),
-                                 input.ClinicalESBL(),
                                  input.RecentHospitalization(),
                                  input.ICUExposure(),
-                                 input.PriorGNresistance(),
-                                 input.PriorSameClassAbx(),
-                                 input.PriorDiffClassAbx()]
+                                 
+                                 input.PriorPenicillin(),
+                                 input.PriorCephalosporin(),
+                                 input.PriorCarbapenem(),
+                                 input.PriorFQ(),
+                                 input.PriorAMG(),
+                                 input.PriorOtherAbx(),
+                                 
+                                 input.PriorCefazolinResistance(),
+                                 input.PriorCeftriaxoneResistance(),
+                                 input.PriorCeftazidimeResistance(),
+                                 input.PriorPiptazResistance(),
+                                 input.PriorMeropenemResistance(),
+                                 input.PriorCiprofloxacinResistance(),
+                                 input.PriorTobramycinResistance(),
+                                 input.PriorTMPSMXResistance(),
+                                 
+                                 input.ClinicalESBL()
+                                ]
+            df = pd.DataFrame(columns=regression_inputs)
+            df.loc[len(df)] = regression_values
+                        
+            susceptibility_outputs.append((antibiotic,reg.predict_proba(df)[0][0]*100))
+        
+        # Sort antibiotics by most to least susceptible
+        susceptibility_outputs.sort(key=lambda x: x[1],reverse=True)
+        
+        # Convert to a pandas dataframe and add column names
+        df = pd.DataFrame(susceptibility_outputs)
+        df = df.rename(columns={0:'Antibiotic',1:'Predicted susceptibility (%)'})
+        
+        # Rename 'Piptaz_or_Tobramycin' to 'Dual piptaz + tobramycin'
+        df = df.replace('Piptaz_or_Tobramycin','Dual piptaz + tobramycin')
+        
+        # Round numbers to 1 digits before outputting
+        return(df.round(1))
+    
+
+    @output
+    @render.plot
+    
+    def plot():
+        
+        susceptibility_outputs = []
+
+        for antibiotic in antibiotic_list:
+                        
+            with open(antibiotic+'.pickle','rb') as f:
+                reg = pickle.load(f)
+                    
+
+            regression_values = [input.Hospital(),
+                                 input.Age(),
+                                 input.SexCat(),
+                                 input.MedVsSurgAdmission(),
+                                 input.RecentHospitalization(),
+                                 input.ICUExposure(),
+                                 
+                                 input.PriorPenicillin(),
+                                 input.PriorCephalosporin(),
+                                 input.PriorCarbapenem(),
+                                 input.PriorFQ(),
+                                 input.PriorAMG(),
+                                 input.PriorOtherAbx(),
+                                 
+                                 input.PriorCefazolinResistance(),
+                                 input.PriorCeftriaxoneResistance(),
+                                 input.PriorCeftazidimeResistance(),
+                                 input.PriorPiptazResistance(),
+                                 input.PriorMeropenemResistance(),
+                                 input.PriorCiprofloxacinResistance(),
+                                 input.PriorTobramycinResistance(),
+                                 input.PriorTMPSMXResistance(),
+                                 
+                                 input.ClinicalESBL()
+                                ]
                                  
             df = pd.DataFrame(columns=regression_inputs)
             df.loc[len(df)] = regression_values
             
-            susceptibility = reg.predict_proba(df)[0][0]
-
-            return ('''Antibiotic:                    {}
-Probability of susceptibility: {:.1f}%'''.format(input.antibiotic(),susceptibility*100))
-        
-        # Case of a combination antibiotic
-        else: 
+            prediction = reg.predict_proba(df)[0][0]
             
-            regression_inputs = ['Age','SexCat','MedVsSurgAdmission','ClinicalESBL',
-                                 'RecentHospitalization','ICUExposure',
-                                 'PriorGNresistance',
-                                 'PriorPenicillin','PriorAMG','PriorNonPenicillin','PriorNonAMG']
-            with open('../pickle_files/'+input.antibiotic()+'.pickle','rb') as f:
-                reg = pickle.load(f)
-                
-
-            regression_values = [input.Age(),
-                                 input.SexCat(),
-                                 input.MedVsSurgAdmission(),
-                                 input.ClinicalESBL(),
-                                 input.RecentHospitalization(),
-                                 input.ICUExposure(),
-                                 input.PriorGNresistance(),
-                                 input.PriorPenicillin(),
-                                 input.PriorAMG(),
-                                 input.PriorNonPenicillin(),
-                                 input.PriorNonAMG()
-                                ]
-                             
-            df = pd.DataFrame(columns=regression_inputs)
-            df.loc[len(df)] = regression_values
+            # Rename 'Piptaz_or_Tobramycin' to 'Dual piptaz + tobramycin'
+            if antibiotic == 'Piptaz_or_Tobramycin':
+                antibiotic = 'Dual piptaz + tobramycin'
+            susceptibility_outputs.append((antibiotic,prediction))
             
-            susceptibility = reg.predict_proba(df)[0][0]                     
-            return ('''Antibiotics:                   {}
-Probability of susceptibility: {:.1f}%'''.format(input.antibiotic(),susceptibility*100))
-            print('doeo')
+            # susceptibility_outputs.append((antibiotic,reg.predict_proba(df)[0][0]))
         
+        # Create a plot       
+        fig, ax = plt.subplots()
+        ax.clear()
+        
+        # Format the plot
+        ax.set_xlabel('Antibiotic')
+        ax.set_ylabel('Susceptibility (%)')
+        ax.set_ylim(0,100)
+        ax.tick_params('x',rotation = 80)
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+  
+        # Sort antibiotics by most to least susceptible
+        susceptibility_outputs.sort(key=lambda x: x[1],reverse=True)
+        
+        x = [i[0] for i in susceptibility_outputs]
+        y = [i[1]*100 for i in susceptibility_outputs]
+        
+        # Plot points
+        bars = ax.bar(x,y)
+        ax.bar_label(bars,fmt='')        
+        ax.bar_label(bars,fmt='%d')
+        
+        # Plot an 80% or 90% horizontal line based on clinical severity
+        cutoff = int(input.Severity())
+        plt.axhline(y=cutoff,color='orange',label='{}%'.format(cutoff))
+        
+        ax.legend()
+        
+        return fig
+    
+    
 app = App(app_ui, server)
+
